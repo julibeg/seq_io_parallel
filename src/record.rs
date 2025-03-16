77721daecc1ee44a4d89fa118c124c60d58be7dd
Julian Libiseller-Egger <37619875+julibeg@users.noreply.github.com>
2025-03-16 23:12:37 +0100

    add ref_seq_lines

--- src/record.rs
+++ src/record.rs
@@ -7,6 +7,8 @@ pub trait MinimalRefRecord<'a> {
 
     fn ref_full_seq(&self) -> Cow<[u8]>;
 
+    fn ref_seq_lines(&self) -> impl Iterator<Item = &'_[u8]>;
+
     fn ref_qual(&self) -> &[u8];
 }
 
@@ -21,6 +23,11 @@ impl MinimalRefRecord<'_> for seq_io::fastq::RefRecord<'_> {
 
     fn ref_full_seq(&self) -> Cow<[u8]> {
         Cow::Borrowed(self.ref_seq())
+    }
+
+    fn ref_seq_lines(&self) -> impl Iterator<Item = &'_[u8]> {
+        // simply return an iterator yielding the full sequence
+        std::iter::once(self.ref_seq())
     }
 
     fn ref_qual(&self) -> &[u8] {
@@ -39,6 +46,10 @@ impl MinimalRefRecord<'_> for seq_io::fasta::RefRecord<'_> {
 
     fn ref_full_seq(&self) -> Cow<[u8]> {
         self.full_seq()
+    }
+
+    fn ref_seq_lines(&self) -> impl Iterator<Item = &'_[u8]> {
+        Box::new(self.seq_lines())
     }
 
     fn ref_qual(&self) -> &[u8] {
